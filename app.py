ab461268c28dcfd0578cd8ee7491f91f9dd8e4a2
Filip <philippo.sanecki@gmail.com>
2023-12-13 20:33:30 +0100

    First fully functional version

--- app.py
+++ app.py
@@ -1,72 +1,103 @@
 from flask import Flask
-from dash import Dash, html, dcc, Input, Output, State
+from dash import Dash, html, dcc, Input, Output
 import plotly.graph_objs as go
 import plotly.express as px
 import requests
 import pandas as pd
 import pandas_ta as ta
 import json
+from datetime import date, timedelta
+from random import randint
+from constants import API_KEY
 
+# Function to get news from MarketAux API
+
+def get_news():
+    yesterday = date.today() - timedelta(days=1)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+
+    url = f"https://api.marketaux.com/v1/news/all?industries=Technology&filter_entities=true&limit=10&published_after={yesterday}T17:47&api_token={API_KEY}"
+
+    response = requests.get(url)
+    json_data = response.json()
+    choice = randint(0, len(json_data["data"])-1)
+    if json_data["data"][choice]["description"] == None:
+        json_data["data"][choice]["description"] = "No description available"
+
+    elif json_data["data"][choice]["title"] == None:
+        json_data["data"][choice]["title"] = "No title available"
+        
+    elif json_data["data"][choice]["source"] == None:
+        json_data["data"][choice]["source"] = "No source available"
+    
+    return json_data, choice
+
+json_data, choice = get_news()
 
 
 
 server = Flask(__name__)
 
-
-with open("pairs.json", "r", encoding="utf-8") as f:
+with open("pairs.json", "r") as f:
     list_of_currencies = json.load(f)
  
-
+list_of_currencies = [c.upper() for c in list_of_currencies]
 
 with open('grouped_indicators.json', 'r') as file:
     grouped_indicators = json.load(file)
 
+time_dict = {
+    'minute': 60,
+    'hour': 3600,
+    'day': 86400
+}
+
 indicator_list = [k for k in grouped_indicators.keys()]
-
-
+indicator_list = [i.upper() for i in indicator_list]
 
 app = Dash(
     __name__,
     server=server,
     url_base_pathname='/dash/',
     assets_folder='./assets/',
+    update_title=None
 )
-
-
+app.title = "MyStock"
 
 app.config.suppress_callback_exceptions = True
 
-
 app.layout = html.Div(id='main', className="main-div", children=[
-    
-    
-        html.Div(id='charts', className="charts", children=[
+    html.Div(id='charts', className="charts", children=[
         dcc.Graph(id='candles', className="candles"),
         dcc.Graph(id='indicator', className="indicator"),
         dcc.Graph(id='volume',  className="volume"),
         dcc.Graph(id='indicator2', className="indicator2"),
         dcc.Interval(id='interval', interval=2000)
-            ]), 
+    ]), 
         
-        html.Div(id='menu', className="menu-div", children=[
-            html.H1("Crypto Dashboard" , className="title"),
-            html.H3("Select a currency:", className="title2"),
-            dcc.Dropdown(list_of_currencies, id ="coin-select", className="coin-select", value = "btcusd"),
-            html.H3("Select first indicator:", className="title3"),
-            dcc.Dropdown(indicator_list, id ="indicator-select", className="indicator-select", value = "rsi"),
-            html.H3("Select second indicator:"),
-            dcc.Dropdown(indicator_list, id ="indicator-select2", className="indicator-select2", value = "rsi"),
-            html.H3("Click to draw line:"),
-            html.Button('Draw', id='draw', className="draw"),
-            
-        ]),
-])
+    html.Div(id='menu', className="menu-div", children=[
+        html.H1("Dashboard" , className="title"),
+        html.H3("Select a currency:", className="title2"),
+        dcc.Dropdown(list_of_currencies, id ="coin-select", className="coin-select", value = "btcusd"),
+        html.H3("Select first indicator:", className="title3"),
+        dcc.Dropdown(indicator_list, id ="indicator-select", className="indicator-select", value = "rsi"),
+        html.H3("Select second indicator:"),
+        dcc.Dropdown(indicator_list, id ="indicator-select2", className="indicator-select2", value = "rsi"),
+        html.H3("Select time interval:"),
+        dcc.Dropdown([k for k in time_dict.keys()] ,id ="frequency-select3", className="frequency-select3", value = "day"),
+        html.Div(id='news', className="news", children=[
+            html.H4("News:", className="title4"),
+            html.H5(f"{json_data['data'][choice]['title']}"),
+            html.H5(f" {json_data['data'][choice]['description']}"),
+            html.Plaintext(f"Source: {json_data['data'][choice]['source']}")
+        ], style={'display': 'flex', 'font-family':'Roboto, sans-serif', "border": "1px solid black"})
+    ])
+], style={'display': 'flex', 'font-family':'Roboto, sans-serif'})
                  
                           
         
 
 @app.callback(
-    
     Output('candles', 'figure'),
     Output('indicator', 'figure'),
     Output('indicator2', 'figure'),
@@ -75,20 +106,21 @@ app.layout = html.Div(id='main', className="main-div", children=[
     Input('coin-select', 'value'),
     Input('indicator-select', 'value'),
     Input('indicator-select2', 'value'),
+    Input('frequency-select3', 'value'),
     Input('interval', 'n_intervals'),
-    
-    
 )
 
-def update_graph(value, indicator1, indicator2, n_intervals):
+def update_graph(value, indicator1, indicator2, frequency,  n_intervals):
     
-    url = f"https://www.bitstamp.net/api/v2/ohlc/{value}/"
+    url = f"https://www.bitstamp.net/api/v2/ohlc/{value.lower()}/"
     
     params = {
-        "step": 60,
+        "step": time_dict[frequency],
         "limit": 60,
     }
-    
+    indicator1 = indicator1.lower()
+    indicator2 = indicator2.lower()
+
     function1 = getattr(ta, indicator1)
     function2 = getattr(ta, indicator2)
     
@@ -127,48 +159,128 @@ def update_graph(value, indicator1, indicator2, n_intervals):
             )
         ],
         layout=go.Layout(
-        title=go.layout.Title(text=f"{value}")
+        title=go.layout.Title(text=f"{value.upper()}",
+        )
     )
         
     )
-    candles.update_layout(xaxis_rangeslider_visible=False, height=515, width=750)
+    candles.update_layout(
+        xaxis_rangeslider_visible=False, 
+        xaxis_title="Date", 
+        yaxis_title="Price", 
+        plot_bgcolor="white", 
+        paper_bgcolor="whitesmoke"
+        )
+
+    candles.update_xaxes(
+    mirror=True,
+    ticks='outside',
+    showline=True,
+    linecolor='black',
+    gridcolor='lightgrey',
+    title_text="Date"
+    )
+
+    candles.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Price"
+    )
     
+    
+
     indicator = px.line(
         x=data.timestamp,
         y=data[indicator1],
-        title=f"First indicator: {indicator1} Description",
-        height=300,
+        title=f"First indicator: {indicator1.upper()}",
+        
         
         
     )
-    indicator.update_layout(xaxis_rangeslider_visible=False, height=515, width=750)
-    indicator.update_yaxes(title_text=None)  # Remove y-axis title
-    indicator.update_xaxes(title_text=None)
+    indicator.update_layout(xaxis_rangeslider_visible=False, plot_bgcolor="white", paper_bgcolor="whitesmoke")
+    indicator.update_xaxes(
+    mirror=True,
+    ticks='outside',
+    showline=True,
+    linecolor='black',
+    gridcolor='lightgrey',
+    title_text="Value"
+    )
+    indicator.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Date"
+    )
+   
     
     indicator2 = px.line(
         x=data.timestamp,
         y=data[indicator2],
-        title=f"Second indicator: {indicator2}",
-        height=300,
+        title=f"Second indicator: {indicator2.upper()}",
+       
         
     )
-    indicator2.update_layout(xaxis_rangeslider_visible=False, height=515, width=750)
-    indicator2.update_yaxes(title_text=None)  # Remove y-axis title
-    indicator2.update_xaxes(title_text=None)
+    indicator2.update_layout(
+        xaxis_rangeslider_visible=False, 
+        plot_bgcolor="white", 
+        paper_bgcolor="whitesmoke"
+    )
+    
+    indicator2.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Value"
+    )
+
+    indicator2.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Date"
+    )
     
     
     volume = px.bar(
         x=data.timestamp,
         y=data.volume,
         title="Volume",
-        height=300,
     )
-    volume.update_layout(xaxis_rangeslider_visible=False, height=515, width=750)
-    volume.update_layout(autotypenumbers='convert types')
-    volume.update_yaxes(title_text="Quantity")  # Update y-axis title to "Quantity"
-    
-    
 
+    volume.update_layout(
+        xaxis_rangeslider_visible=False, 
+        autotypenumbers='convert types', 
+        plot_bgcolor="white", 
+        paper_bgcolor="whitesmoke"
+    )
+
+    volume.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Date"
+    )
+
+    volume.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        title_text="Volume"
+    )
     return candles, indicator, indicator2, volume
 
 
